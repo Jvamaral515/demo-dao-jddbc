3aa76915838751398cf3273d34fbae805dc22ae7
Joao Victor <jv-amaralds@hotmail.com>
2024-01-17 14:57:41 -0300

    delete implementation

--- src/db/DbIntegrityException.java
+++ src/db/DbIntegrityException.java
@@ -5,7 +5,7 @@ public class DbIntegrityException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
 	public DbIntegrityException(String msg) {
-		super();
+		super(msg);
 	}
 
 }
